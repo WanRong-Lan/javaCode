fb604f22c2dfdd55dd423a1e8f3c7cf674bc2d7e
LanWanRong <wanrong.lan@worldlinking.org>
2021-05-12 22:48:41 +0800

    # Java - Thread 简单实现，线程操作--等候、唤醒。  wait、notify、notifyAll

--- src/sample/java/thread/ThreadInputOutputDome.java
+++ src/sample/java/thread/ThreadInputOutputDome.java
@@ -1,8 +1,24 @@
 package sample.java.thread;
 // 线程数据通信
+
+/**
+ * wait、notify、notifyAll
+ *  都使用在同步中的，因为对持有监视锁的线程操作，所以要在同步中。
+ *  因为只有同步才具有锁。
+ *
+ *  为什么这些线程操作的方法定义在Object类中？
+ *  因为这些方法在操作同步线程时，
+ *  都必须要表示它们所操作线程只有的锁，
+ *  只有同一个锁上的被等待线程，可以被同一个锁上的notify唤醒。
+ *  不可以对不同锁中的线程进行唤醒。
+ *  简单来锁，等待和唤醒必须是同一个锁。
+ *  而锁可以是任意对象，所以可以被任意对象调用的方法定义Object类
+ */
 class People{
     private String name;
     private String sex;
+    // 执行标记，false 为写入，true为输出
+    private Boolean flag = false;
 
     public void setName(String name) {
         this.name = name;
@@ -27,6 +43,13 @@ class People{
     public String getName() {
         return name;
     }
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
 }
 class Input implements Runnable{
     private People people;
@@ -40,6 +63,14 @@ class Input implements Runnable{
         int i = 0;
         while (true){
             synchronized (this.people){
+                // 标记为true，进程等候
+                if(this.people.getFlag()) {
+                    try {
+                        this.people.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 if(i%2!=0){
                     this.people.setName("丽丽");
                     this.people.setSex("女");
@@ -47,7 +78,14 @@ class Input implements Runnable{
                     this.people.setName("亮亮");
                     this.people.setSex("男");
                 }
+                this.people.setFlag(true);
                 i++;
+                // 唤醒线程池下一个线程
+                try {
+                    this.people.notify();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
 
         }
@@ -64,7 +102,21 @@ class Output implements Runnable{
     public void run() {
         while (true){
             synchronized (this.people){
+                if(!this.people.getFlag()) {
+                    try {
+                        this.people.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 System.out.println(this.people.toString());
+                this.people.setFlag(false);
+                try {
+                    this.people.notify();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
             }
         }
     }
