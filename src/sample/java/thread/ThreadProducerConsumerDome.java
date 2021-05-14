fa9793d872f20c8385e3a38250707d74c859a7fa
LanWanRong <wanrong.lan@worldlinking.org>
2021-05-14 21:54:25 +0800

    # Java - 多线程操作 -- 显式的方式进行多闲操作：lock、unlock、condition。 优化：生产或消费线程 在最后只唤醒了对象。减少唤醒线程数导致的资源消耗

--- src/sample/java/thread/ThreadProducerConsumerDome.java
+++ src/sample/java/thread/ThreadProducerConsumerDome.java
@@ -5,11 +5,19 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 结合 ThreadInputOutPutDome.class 在有2个以上线程以上是容易出现Input和Output 两个重复执行，打断了交替执行。
+ * 结合 ThreadInputOutPutDome.class 在有2个以上线程以上是容易出现Input和Output
  *
- * 生成者--消费者
+ * 生成者--消费者  ----  显式锁机制
+ * 通过condition 操作唤醒和沉睡指定的线程。
+ *
+ * 实现了将同步 synchronized 函数替换成实现Lock 操作。
+ * 由原来的Object中的wait、notify、notifyAll 替换成了condition对象。
+ * 优化了唤醒全部线程导致的无用资源的消耗，指定了生产者线程或消费者线程 只唤醒对方。
+ *
+ * 在该示例中，实现了本方只唤醒对方的操作
  */
 public class ThreadProducerConsumerDome {
+
     public static void main(String[] args) {
         Resoutce res = new Resoutce();
         // 生产者线程
@@ -34,22 +42,28 @@ class Resoutce{
     private int count = 1;
     private boolean flag = false;
     private Lock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
+
+    // 生产者 condition
+    private Condition condition_pro = lock.newCondition();
+    // 消费者 condition
+    private Condition condition_com=lock.newCondition();
+
     public  void setInfo(String name) throws InterruptedException {
         // 获取锁
         lock.lock();
         try{
             // 没有while 判断，和notifyAll 唤醒所有线程 则所有线程就会等待
             while (flag)
-                condition.await();
+                // 生产者要等待
+                condition_pro.await();
 
             this.name="产品名称："+name+"，产品编号："+count++;
             System.out.println(Thread.currentThread().getName()+":生成："+this.name);
             this.flag=true;
-            // 唤醒一个等待线程
-            condition.signal();
+            // 唤醒一个消费者
+            condition_com.signal();
         }finally {
-            // 必须执行的
+            // 必须执行的，所以加入到了finally中
             lock.unlock();// 解锁
         }
 
@@ -58,10 +72,12 @@ class Resoutce{
         lock.lock();
         try{
             while (!flag)
-                condition.await();
+                // 消费者等待
+                condition_com.await();
             System.out.println(Thread.currentThread().getName()+":消费......"+this.name);
             this.flag=false;
-            condition.signal();
+            // 唤醒一个生产者
+            condition_pro.signal();
         }finally {
             lock.unlock();
         }
